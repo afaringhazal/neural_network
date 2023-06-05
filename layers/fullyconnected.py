eaa85fbce53dc6e6323b0fc7b46e88b8c5768d00
afaringhazal <Afaringhazal@gmail.com>
2023-06-05 22:24:11 +0330

    complete FC

--- layers/fullyconnected.py
+++ layers/fullyconnected.py
@@ -40,7 +40,7 @@ class FC(Abs_layer):
     def initialize_bias(self):
         # TODO: Initialize bias with zeros
         val = np.zeros((1, self.output_size))
-        print(f'bias :\n {val}')
+        # print(f'bias :\n {val}')
         return val
     
     def forward(self, A_prev):
@@ -57,14 +57,18 @@ class FC(Abs_layer):
         A_prev_tmp = np.copy(A_prev)
 
         # TODO: Implement forward pass for fully connected layer
-        # if None: # check if A_prev is output of convolutional layer
-        #     batch_size = None
-        #     A_prev_tmp = A_prev_tmp.reshape(None, -1).T
-        # self.reshaped_shape = A_prev_tmp.shape
+        if len(A_prev_tmp.shape) == 4: # check if A_prev is output of convolutional layer
+            # print("-----------------------------------")
+            count_of_features = A_prev_tmp.shape[1] * A_prev_tmp.shape[2] * A_prev_tmp.shape[3]
+            A_prev_tmp = A_prev_tmp.reshape(count_of_features, -1).T
+        self.reshaped_shape = A_prev_tmp.shape
         
         # TODO: Forward part
         W, b = self.parameters
-        Z = A_prev @ W + b
+        # print(f'A_prev shape {A_prev_tmp.shape}')
+        # print(f'b.shape {b.shape}')
+        # print(f'w.shape {W.shape}')
+        Z = A_prev_tmp @ W + b
         return Z
     
     def backward(self, dZ, A_prev):
@@ -78,19 +82,19 @@ class FC(Abs_layer):
                 grads: list of gradients for the weights and bias
         """
         A_prev_tmp = np.copy(A_prev)
-        # if None: # check if A_prev is output of convolutional layer
-        #     batch_size = None
-        #     A_prev_tmp = A_prev_tmp.reshape(None, -1).T
+        if len(A_prev_tmp.shape) == 4:  # check if A_prev is output of convolutional layer
+            count_of_features = A_prev_tmp.shape[1] * A_prev_tmp.shape[2] * A_prev_tmp.shape[3]
+            A_prev_tmp = A_prev_tmp.reshape(count_of_features, -1).T
 
         # TODO: backward part
         W, b = self.parameters
-        dW = A_prev.T @ dZ / A_prev.shape[0]
-        db = np.sum(dZ, axis=0, keepdims=True) / A_prev.shape[0]
+        dW = A_prev_tmp.T @ dZ / A_prev_tmp.shape[0]
+        db = np.sum(dZ, axis=0, keepdims=True) / A_prev_tmp.shape[0]
         dA_prev = dZ @ W.T
         grads = [dW, db]
         # reshape dA_prev to the shape of A_prev
-        # if None:    # check if A_prev is output of convolutional layer
-        #     dA_prev = dA_prev.T.reshape(self.input_shape)
+        if len(A_prev.shape) == 4:    # check if A_prev is output of convolutional layer
+            dA_prev = dA_prev.T.reshape(self.input_shape)
         return dA_prev, grads
     
     def update(self, optimizer, grads):
@@ -100,4 +104,4 @@ class FC(Abs_layer):
                 optimizer: optimizer object
                 grads: list of gradients for the weights and bias
         """
-        self.parameters = optimizer.update(grads, self.name, self.parameters)
+        self.parameters = optimizer.update(grads, self.name)
